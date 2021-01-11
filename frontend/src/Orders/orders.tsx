21ca91db0716609d39ded9c47e3ba2b7e19231d7
Tef <thominhaserick@gmail.com>
2021-01-10 22:17:04 -0300

    Create product List

--- frontend/src/Orders/orders.tsx
+++ frontend/src/Orders/orders.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react'
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import './orders.css'
 import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
+import { Product } from './types';
+
 
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, [])
+
+
     return (
         <div className='orders-container'>
             <StepsHeader />
-            <ProductsList />
+            <ProductsList products={products} />
+            <OrderLocation />
         </div>
     )
 
