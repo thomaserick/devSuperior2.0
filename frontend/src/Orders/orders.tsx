170d35b5bac0d4506463dca4b26fc9e07030f5f4
Tef <thominhaserick@gmail.com>
2021-01-11 22:30:43 -0300

    Create label summary

--- frontend/src/Orders/orders.tsx
+++ frontend/src/Orders/orders.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react'
 import { fetchProducts } from '../api';
+import Footer from '../Footer/footer';
 import OrderLocation from './OrderLocation';
 import './orders.css'
+import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import { OrderLocationdata, Product } from './types';
-
-
 
 
 function Orders() {
@@ -22,11 +22,15 @@ function Orders() {
 
 
     return (
-        <div className='orders-container'>
-            <StepsHeader />
-            <ProductsList products={products} />
-            <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
-        </div>
+        <>
+            <div className='orders-container'>
+                <StepsHeader />
+                <ProductsList products={products} />
+                <OrderLocation onChangeLocation={location => setOrderLocation(location)} />
+                <OrderSummary />
+            </div>
+            <Footer />
+        </>
     )
 
 }
